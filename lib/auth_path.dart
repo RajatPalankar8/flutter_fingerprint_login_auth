af4148e434fdc928787b94416c71ebeda49304a9
rajatpalankar <rajatrrpalankar@gmail.com>
2021-09-19 20:57:52 +0530

    first push

---
+++ lib/auth_path.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:local_auth_fingerprint/home_page.dart';
+
+class AuthPath extends StatefulWidget {
+  const AuthPath({Key? key}) : super(key: key);
+
+  @override
+  _AuthPathState createState() => _AuthPathState();
+}
+
+class _AuthPathState extends State<AuthPath> {
+
+  bool? _hasBioSensor;
+
+  LocalAuthentication authentication = LocalAuthentication();
+
+
+  Future<void> _checkBio() async{
+    try{
+      _hasBioSensor  = await authentication.canCheckBiometrics;
+
+      print(_hasBioSensor);
+
+      if(_hasBioSensor!){
+        _getAuth();
+      }
+
+    }on PlatformException catch(e){
+      print(e);
+    }
+  }
+
+  Future<void> _getAuth() async{
+    bool isAuth = false;
+    try{
+      isAuth = await authentication.authenticate(
+          localizedReason: 'Scan your finger print to access the app',
+           biometricOnly: true,
+        useErrorDialogs: true,
+        stickyAuth: true
+      );
+
+      if(isAuth){
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (builder)=>HomePage()));
+      }
+
+      print(isAuth);
+    }on PlatformException catch(e){
+      print(e);
+    }
+
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _checkBio();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Flutter Local Fingerprint Auth',
+            style: TextStyle(fontSize: 30),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              height: 70,
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                  child: const Text('Check Auth'),
+                  onPressed: () {
+                    _checkBio();
+                  },
+                  style: ElevatedButton.styleFrom(
+                      shape: const StadiumBorder(), primary: Colors.green))),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:local_auth_fingerprint/auth_path.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      
+        primarySwatch: Colors.blue,
+      ),
+      home: AuthPath()
+    );
+  }
+}
+
+
+
